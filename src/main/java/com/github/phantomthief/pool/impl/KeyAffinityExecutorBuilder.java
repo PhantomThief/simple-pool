fbb3a84f6ed6b6b09af6b18881268649ca4b5fe7
w.vela <wangtianzhou@kuaishou.com>
2018-11-30 14:07:16 +0800

    [refactor] make impl a top-level class.

--- src/main/java/com/github/phantomthief/pool/impl/KeyAffinityExecutorBuilder.java
+++ src/main/java/com/github/phantomthief/pool/impl/KeyAffinityExecutorBuilder.java
@@ -16,11 +16,8 @@ import java.util.function.Supplier;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
-import com.github.phantomthief.pool.KeyAffinity;
 import com.github.phantomthief.pool.KeyAffinityExecutor;
-import com.github.phantomthief.pool.KeyAffinityExecutorStats;
 import com.google.common.util.concurrent.ListeningExecutorService;
 
 /**
@@ -30,7 +27,7 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class KeyAffinityExecutorBuilder {
 
-    private static final Map<KeyAffinityExecutor<?>, KeyAffinityExecutor<?>> ALL_EXECUTORS = new ConcurrentHashMap<>();
+    static final Map<KeyAffinityExecutor<?>, KeyAffinityExecutor<?>> ALL_EXECUTORS = new ConcurrentHashMap<>();
     private final KeyAffinityBuilder<ListeningExecutorService> builder = new KeyAffinityBuilder<>();
 
     private boolean shutdownAfterClose = true;
@@ -41,7 +38,7 @@ public class KeyAffinityExecutorBuilder {
             builder.depose(it -> shutdownAndAwaitTermination(it, 1, DAYS));
         }
         builder.ensure();
-        ExecutorImpl<K> result = new ExecutorImpl<>(builder::buildInner);
+        KeyAffinityExecutorImpl<K> result = new KeyAffinityExecutorImpl<>(builder::buildInner);
         ALL_EXECUTORS.put(result, wrapStats(result));
         return result;
     }
@@ -92,38 +89,5 @@ public class KeyAffinityExecutorBuilder {
 
     public static Collection<KeyAffinityExecutor<?>> getAllExecutors() {
         return unmodifiableCollection(ALL_EXECUTORS.values());
-    }
-
-    private class ExecutorImpl<K> extends LazyKeyAffinity<K, ListeningExecutorService> implements
-                              KeyAffinityExecutor<K> {
-
-        ExecutorImpl(Supplier<KeyAffinity<K, ListeningExecutorService>> factory) {
-            super(factory);
-        }
-
-        @Override
-        public void close() throws Exception {
-            try {
-                super.close();
-            } finally {
-                ALL_EXECUTORS.remove(this);
-            }
-        }
-
-        @Nullable
-        @Override
-        public KeyAffinityExecutorStats stats() {
-            int parallelism = 0;
-            int activeCount = 0;
-            for (ListeningExecutorService executor : this) {
-                if (executor instanceof ThreadListeningExecutorService) {
-                    parallelism += ((ThreadListeningExecutorService) executor).getMaximumPoolSize();
-                    activeCount += ((ThreadListeningExecutorService) executor).getActiveCount();
-                } else {
-                    throw new IllegalStateException("cannot get stats for " + this);
-                }
-            }
-            return new KeyAffinityExecutorStats(parallelism, activeCount);
-        }
     }
 }
