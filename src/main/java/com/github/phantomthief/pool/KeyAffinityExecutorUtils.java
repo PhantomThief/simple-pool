c495822cec8d577fa20eea104c4fad742e83c587
w.vela <boywang004@hotmail.com>
2020-08-20 16:08:10 +0800

    feat(executor): add dynamic capacity linked blocking queue. (#9)

--- src/main/java/com/github/phantomthief/pool/KeyAffinityExecutorUtils.java
+++ src/main/java/com/github/phantomthief/pool/KeyAffinityExecutorUtils.java
@@ -2,12 +2,15 @@ package com.github.phantomthief.pool;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
+import com.github.phantomthief.pool.impl.DynamicCapacityLinkedBlockingQueue;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
@@ -17,6 +20,32 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 public class KeyAffinityExecutorUtils {
 
     public static final int RANDOM_THRESHOLD = 20;
+
+    static Supplier<ExecutorService> executor(String threadName, IntSupplier queueBufferSize) {
+        return new Supplier<ExecutorService>() {
+
+            private final ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                    .setNameFormat(threadName)
+                    .build();
+
+            @Override
+            public ExecutorService get() {
+                BlockingQueue<Runnable> queue = new DynamicCapacityLinkedBlockingQueue<Runnable>(queueBufferSize) {
+                    @Override
+                    public boolean offer(Runnable o) {
+                        try {
+                            put(o);
+                            return true;
+                        } catch (InterruptedException ie) {
+                            Thread.currentThread().interrupt();
+                        }
+                        return false;
+                    }
+                };
+                return new ThreadPoolExecutor(1, 1, 0L, MILLISECONDS, queue, threadFactory);
+            }
+        };
+    }
 
     static Supplier<ExecutorService> executor(String threadName, int queueBufferSize) {
         return new Supplier<ExecutorService>() {
